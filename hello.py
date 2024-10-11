7f49e4d64a5de12c7adfb0cf9c33f0788bd08dab
Felix Cv <C22482266@mytudublin.ie>
2024-10-11 23:19:37 +0100

    updated hello.py file

--- hello.py
+++ hello.py
@@ -3,3 +3,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<p>Hello, World!</p>'
+@app.route('/about')
+def about():
+    return '<p>This is a Flask web app running in a Linux VM.</p>'
